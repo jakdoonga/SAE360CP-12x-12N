15c4793d3b9d4ad01bce229ef3357fa140470ae1
jakdoonga <88531120+jakdoonga@users.noreply.github.com>
2023-07-26 16:04:12 +0900

    Add files via upload

---
+++ i2c.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+unsigned char slave_add = 0x36;
+unsigned char angle1_add = 0x0C;
+unsigned char angle2_add = 0x0D;
+
+const long looptime = 10;
+unsigned long prevMicros = 0;
+unsigned long currMicros;
+
+void setup() {
+  Wire.begin();             // Join I2C bus (address is optional for controller device)
+  Serial.begin(9600);       // Start serial for output
+  Serial.println("Master on");
+}
+
+void loop() {
+  currMicros = micros();
+  if (currMicros - prevMicros >= looptime) {
+    prevMicros = currMicros;
+    Wire.beginTransmission(slave_add);
+    Wire.write(angle1_add);
+    Wire.endTransmission(false);
+
+    Wire.beginTransmission(slave_add ^ 0x01);
+    Wire.endTransmission(true);
+
+    Wire.requestFrom(slave_add, 2);    // Request 6 bytes from slave device number two
+
+    if (Wire.available()) {
+      unsigned char answer = Wire.read();
+      unsigned char answer2 = Wire.read();
+      unsigned int real;
+      float real2;
+      real = answer;
+      real <<= 8;
+      real |= answer2;
+      real2 = (float) real / (4096.0 - 1.0) * 360.0;
+      Serial.println(real2);
+      //      Serial.print(answer);
+      //      Serial.print("\t");
+      //      Serial.println(answer2);
+    }
+  }
+}
